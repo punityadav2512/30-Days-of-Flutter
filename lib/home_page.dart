b1c5922e3cfd876b3277b67cc1a28cd0bb49959c
punityadav2512 <b520040@iiit-bh.ac.in>
2021-11-07 14:32:49 +0530

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "Punit";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("catalog App"),
+      ),
+      body: Center(
+        child:
+            Container(child: Text('Welcome to $days days of flutter by $name')),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
